c3c2ded942bd6ed94ab657aa30e15cf18a12d5a5
koen de gruijter <koen.degruijter@student.hu.nl>
2016-06-16 16:17:11 +0200

    removed example module

--- source/exampleModule/exampleModule.cpp
+++
@@ -1,8 +0,0 @@
-#include "exampleModule.hpp"
-
-exampleModule::exampleModule(module_data data, QObject *parent): mapeditorModule(data, parent)
-    {
-		std::cout<< "mod con test" << std::endl;
-		module_name = "example";
-        editTab = new exampleEditTab();
-    }
